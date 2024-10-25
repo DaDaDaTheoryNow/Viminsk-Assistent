1dbd4125944a4ef731af67bb48be540a8282efa3
Nikita0504 <zevnovnikita5@gmail.com>
2024-10-25 21:29:38 +0300

    button

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'widgets/listening_button.dart';
+
 class HomeScreen extends ConsumerWidget {
   static HomeScreen builder(
     BuildContext context,
@@ -13,6 +15,17 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(children: [
+      Align(
+        alignment: Alignment.center,
+        child: Text(
+          'Готов слушать !',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      const Align(alignment: Alignment.bottomCenter, child: ListeningButton()),
+    ]));
   }
 }
