e03d3b3060d8c6460301f30640794da9980416e0
Vladislav <kokgmlollol@gmail.com>
2024-10-26 00:11:00 +0300

    some changes in ui and speech

--- lib/service/speech_to_text/data/datasource/local/speech_to_text_local_datasource_impl.dart
+++ lib/service/speech_to_text/data/datasource/local/speech_to_text_local_datasource_impl.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:viminsk_assistent/service/speech_to_text/data/datasource/local/speech_to_text_local_datasource.dart';
 
 class SpeechToTextLocalDataSourceImpl implements SpeechToTextLocalDataSource {
   final SpeechToText _speechToText = SpeechToText();
+  Timer? _silenceTimer;
 
   @override
   Future<bool> initialize() async {
@@ -16,19 +19,46 @@ class SpeechToTextLocalDataSourceImpl implements SpeechToTextLocalDataSource {
     );
 
     await _speechToText.listen(
-      localeId: "ru_Ru",
+      localeId: "ru_RU",
       listenOptions: options,
       onResult: (result) {
-        onResult(result.recognizedWords);
+        if (result.finalResult) {
+          onResult(result.recognizedWords);
+          _resetSilenceTimer(() => onResult(""));
+        } else {
+          _resetSilenceTimer(() => onResult(""));
+        }
       },
     );
+
+    _startSilenceTimer(() {
+      onResult("");
+      stopListening();
+    });
   }
 
   @override
   Future<void> stopListening() async {
     await _speechToText.stop();
+    _cancelSilenceTimer();
   }
 
   @override
   bool get isListening => _speechToText.isListening;
+
+  void _startSilenceTimer(VoidCallback onStop) {
+    _silenceTimer = Timer(const Duration(milliseconds: 2500), () {
+      onStop();
+    });
+  }
+
+  void _resetSilenceTimer(VoidCallback onStop) {
+    _cancelSilenceTimer();
+    _startSilenceTimer(onStop);
+  }
+
+  void _cancelSilenceTimer() {
+    _silenceTimer?.cancel();
+    _silenceTimer = null;
+  }
 }
