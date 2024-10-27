fec81d1711b41bd953ad16075bc9e3b6a5440cc6
Vladislav <kokgmlollol@gmail.com>
2024-10-28 01:26:30 +0300

    improve ui and ai

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mesh_gradient/mesh_gradient.dart';
 import 'package:viminsk_assistent/features/home/presentation/widgets/sound_level_visualizer.dart';
 import 'package:viminsk_assistent/service/speech_to_text/presentation/provider/speech_to_text_notifier_provider.dart';
 
@@ -39,73 +42,109 @@ class HomeScreen extends ConsumerWidget {
         onStopRecording: () =>
             speechToTextNotifier.stopListening(isForceCancel: true),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Stack(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // sound visualizer
-              AnimatedOpacity(
-                opacity: isLoading
-                    ? 0.0
-                    : isListening
-                        ? 1.0
-                        : 0.0,
-                duration: const Duration(milliseconds: 300),
-                child: Center(
-                  child: SoundLevelVisualizer(
-                    soundLevel: soundLevel,
-                  ),
+          // bg animation
+          AnimatedOpacity(
+            opacity: isListening ? 0.8 : 0.0,
+            duration: const Duration(milliseconds: 1200),
+            curve: Curves.easeIn,
+            child: AnimatedMeshGradient(
+              colors: const [
+                Color.fromARGB(255, 60, 59, 59),
+                Colors.black,
+                Color.fromARGB(255, 111, 109, 109),
+                Colors.black,
+              ],
+              options: AnimatedMeshGradientOptions(
+                amplitude: 2000,
+                frequency: 300,
+                grain: 0.3,
+                speed: 15,
+              ),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
                 ),
               ),
+            ),
+          ),
 
-              // result of question
-              AnimatedOpacity(
-                opacity: isLoading
-                    ? 0.0
-                    : isListening
+          // content
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // sound visualizer
+                  AnimatedOpacity(
+                    opacity: isLoading
                         ? 0.0
-                        : 1.0,
-                duration: const Duration(milliseconds: 250),
-                curve: Curves.easeIn,
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Text(
-                      speechActionResult,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                        : isListening
+                            ? 1.0
+                            : 0.0,
+                    duration: const Duration(milliseconds: 300),
+                    child: Center(
+                      child: SoundLevelVisualizer(
+                        soundLevel: soundLevel,
                       ),
                     ),
                   ),
-                ),
-              ),
 
-              // recognized words
-              AnimatedOpacity(
-                opacity: isLoading
-                    ? 0.0
-                    : isListening
-                        ? 1.0
-                        : 0.0,
-                duration: const Duration(milliseconds: 250),
-                child: Center(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                      recognizedWords.isNotEmpty ? recognizedWords : '',
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                  // result of question
+                  AnimatedOpacity(
+                    opacity: isLoading
+                        ? 0.0
+                        : isListening
+                            ? 0.0
+                            : 1.0,
+                    duration: const Duration(milliseconds: 250),
+                    curve: Curves.easeIn,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Text(
+                          speechActionResult,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+
+                  // recognized words
+                  AnimatedOpacity(
+                    opacity: isLoading
+                        ? 0.0
+                        : isListening
+                            ? 1.0
+                            : 0.0,
+                    duration: const Duration(milliseconds: 250),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            recognizedWords.isNotEmpty ? recognizedWords : '',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
 
           // loading
